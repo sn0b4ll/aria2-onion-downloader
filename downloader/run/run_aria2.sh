1bc07471b40dade9616802a5b6172c347522f079
Dominik Schlecht <dominik.schlecht@hotmail.de>
2021-04-02 17:12:58 +0200

    Spawning 9 Tor-Services on 15001 to 15009

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -6,17 +6,18 @@ touch /log/logs.txt
 
 #echo "forward-socks4a / localhost:${TORPORT} .
 #listen-address 127.0.0.1:${PRIVOPORT}" > /conf/privoxy.conf
-echo "forward-socks4a / localhost:14001 .
-listen-address 127.0.0.1:15001" > /conf/privoxy1.conf
-echo "forward-socks4a / localhost:14002 .
-listen-address 127.0.0.1:15002" > /conf/privoxy2.conf
 
-mkdir -p /var/lib/tor/14001
-mkdir -p /var/lib/tor/14002
-tor --runasdaemon 1 --SOCKSPort 14001 --ControlPort 14101 --DataDirectory /var/lib/14001
-tor --runasdaemon 1 --SOCKSPort 14002 --ControlPort 14102 --DataDirectory /var/lib/14002
-privoxy /conf/privoxy1.conf 
-privoxy /conf/privoxy2.conf 
+max=9
+for i in `seq 1 $max`
+do
+	echo "Creatin $i"
+	echo "forward-socks4a / localhost:1400$i .
+	listen-address 127.0.0.1:1500$i" > /conf/privoxy$i.conf
+
+	mkdir -p /var/lib/tor/1400$i
+	tor --runasdaemon 1 --SOCKSPort 1400$i --ControlPort 1410$i --DataDirectory /var/lib/1400$i
+	privoxy /conf/privoxy$i.conf 
+done
 
 exec aria2c --conf-path=/conf/aria2.conf --log=/log/logs.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET}
 
