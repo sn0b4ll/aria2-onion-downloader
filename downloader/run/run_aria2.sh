f984a7e7f4538418979a493737b9f01446651e12
Dominik Schlecht <dominik.schlecht@hotmail.de>
2021-04-02 14:15:34 +0200

    Working for single downloader

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -4,4 +4,11 @@ set -e
 touch /conf/aria2.session
 touch /log/logs.txt
 
+echo "forward-socks4a / localhost:${TORPORT} .
+listen-address 127.0.0.1:${PRIVOPORT}" > /conf/privoxy.conf
+
+tor --runasdaemon 1 --SOCKSPort ${TORPORT} 
+privoxy /conf/privoxy.conf 
+
 exec aria2c --conf-path=/conf/aria2.conf --log=/log/logs.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET}
+
